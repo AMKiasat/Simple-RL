01fd58920832eeb46956cd8960874d34cc74d054
AMKiasat <kiasatamirm@gmail.com>
2024-02-05 16:53:34 +0330

    Adding movement to robot and point and battery updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 
 
 def state_print(state, p, b, t):
@@ -63,7 +64,7 @@ def calculate_q_table(reward_t, gama):
             for j in range(len(reward_t[i])):
                 if j == 0 and i != 0:
                     table[i][j] = reward_t[i][j] + gama * max(table[i - 1])
-                elif (j == 1) or (j == 0 and i == 0) or (j == 2 and i  == len(reward_t) - 1):
+                elif (j == 1) or (j == 0 and i == 0) or (j == 2 and i == len(reward_t) - 1):
                     table[i][j] = reward_t[i][j] + gama * max(table[i])
                 elif j == 2:
                     table[i][j] = reward_t[i][j] + gama * max(table[i + 1])
@@ -71,22 +72,41 @@ def calculate_q_table(reward_t, gama):
 
 
 if __name__ == '__main__':
-    gama = 0.8
+    gama = 0.5
     actions = ["left", "stay", "right"]
     states = [0, 1, 2, 3, 4, 5]
     point = 0
-    current_state = 1
-    battery_percentage = 100
+    current_state = 4
+    battery_percentage = 30
     trash = True
     print("Guide: robot location= '*', charging station: 'c', trash: 't'")
     while True:
-        # state_print(current_state, point, battery_percentage, trash)
-        battery_percentage -= 5
-        time.sleep(1)
+        state_print(current_state, point, battery_percentage, trash)
+        if current_state == 0:
+            battery_percentage += 10
+            if battery_percentage > 100:
+                battery_percentage = 100
+            trash = True
+            point += 1
+        else:
+            battery_percentage -= 5
+
         reward_table = calculate_reward_table(battery_percentage, trash)
-        # print(reward_table)
         q = calculate_q_table(reward_table, gama)
+        # for i in q:
+        #     print(i)
+
+        best_action = np.argmax(q[current_state])
+
+        if actions[best_action] == "left":
+            current_state += -1
+        elif actions[best_action] == "right":
+            current_state += 1
+        if current_state == 5:
+            trash = False
+            point += 5
 
         if battery_percentage <= 0 and current_state != 0:
             print("Robot shuting down Goodbye!")
             break
+        time.sleep(1)
